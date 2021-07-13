8016b317373d1812326324e90ce0b82b0404f9dd
Muhammad Asim Khaskheli <mkhaskheli.bese18seecs@seecs.edu.pk>
2021-07-13 13:50:38 +0500

    👌 IMPROVE: separate HomePage file added

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,9 +1,22 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget{
+  final int days = 30;
+  final String name="Asim";
   @override
   Widget build(BuildContext context) {
+
     // TODO: implement build
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Hello World"),
+      ),
+      body: Center(
+        child: Container(
+            child: Text("Welcome to $days days of Flutter by $name")
+        ),
+      ),
+      drawer: Drawer(),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:FirstProject/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -8,8 +9,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    int days = 30;
-    String name="Asim";
+
     double pi = 3.14;
     bool isMale = true;
     num temp = 30.5; // num can take int as well as double
@@ -23,13 +23,7 @@ class MyApp extends StatelessWidget {
 
 
       return MaterialApp(
-        home:Material(
-          child: Center(
-            child: Container(
-              child: Text("Welcome to $days days of Flutter by $name")
-            ),
-          ),
-        )
+        home:HomePage()
       );
 
   }
