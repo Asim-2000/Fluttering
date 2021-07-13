841b49a01ad27f8ed91a947653c5c66a4a982418
Muhammad Asim Khaskheli <mkhaskheli.bese18seecs@seecs.edu.pk>
2021-07-13 15:24:42 +0500

    📦 NEW: login page + dart basics

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:FirstProject/home_page.dart';
+import 'package:FirstProject/pages/home_page.dart';
+import 'package:FirstProject/pages/login_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,7 +10,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-
     double pi = 3.14;
     bool isMale = true;
     num temp = 30.5; // num can take int as well as double
@@ -18,14 +18,21 @@ class MyApp extends StatelessWidget {
     var numDays = 5;
 
     const const_pi = 3.14; // Can't change
-    final list = [1,2,3,4]; //See difference between const and final
+    final list = [1, 2, 3, 4]; //See difference between const and final
 
-
-
-      return MaterialApp(
-        home:HomePage()
-      );
-
+    return MaterialApp(
+      // home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      initialRoute: "home",
+      routes: {
+        "/": (context) => Loginpage(),
+        "home": (context) => HomePage(),
+        "/login": (context) => Loginpage()
+      },
+    );
   }
 }
-
---
+++ lib/pages/login_page.dart
@@ -0,0 +1,16 @@
+import "package:flutter/material.dart";
+
+class Loginpage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Material(
+      child: Center(
+        child: Text(
+          "Login Page",
+          style: TextStyle(fontSize: 20, color: Colors.blue,fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
